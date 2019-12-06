42cb718eca9a2b1579e2721bd5482c60084e3942
Sra1th <srya5728@finalproject.c.final-project-260319.internal>
2019-12-06 06:59:06 +0000

    Kafka working code

--- kafkaSparkStream/consumer.py
+++ kafkaSparkStream/consumer.py
@@ -1,15 +1,22 @@
 from kafka import KafkaConsumer, KafkaProducer
-
+from json import loads
 
 if __name__ == '__main__':
 
     print('Running Consumer..')
-    topic_name = 't1'
+    topic_name = 'testTopic'
 
     consumer = KafkaConsumer(topic_name, auto_offset_reset='latest',
-                             bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=10000000)
+                             bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=10000000,
+                             value_deserializer=lambda x: loads(x.decode('utf-8')))
     while(True):
-        msg = consumer._poll_once(timeout_ms=100, max_records=1)
-        if (msg):
-            for key,value in msg.items():
-                    print(value[0].value)
+        # msg = consumer._poll_once(timeout_ms=100, max_records=1)
+        # if (msg):
+        #     for key,value in msg.items():
+        #             print(value[0].value)
+        for msg in consumer:
+            print("Before message:", msg.value)
+	    msg1=msg.value
+            msg1[0]=msg1[0]+10
+            msg1[1]=msg1[1]+10
+            print("After message:", msg1)
---
+++ kafkaSparkStream/kafka_stream_file.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+import sys
+from json import loads
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark import SparkConf
+from pyspark.streaming.kafka import KafkaUtils
+
+def print_stream(x):
+    print("Inside stream:",loads(x[1])[0]+1)
+
+#sparkConf.set("spark.streaming.concurrentJobs", "2")
+
+def print_stream1(x):
+    print("Stream2:",loads(x[1])[0]+1)
+
+if __name__ == "__main__":
+    #conf=SparkConf.setAppName("PythonkafkaWordCount")#.set("spark.streaming.concurrentJobs", "2")
+    sc=SparkContext(appName="PythonkafkaWordCount")
+    ssc=StreamingContext(sc,10)
+    #conf=SparkConf.set("spark.streaming.concurrentJobs", "2").setAppName("PythonkafkaWordCount")
+    sc.setLogLevel("OFF")
+    brokers,topic=sys.argv[1:]
+    kvs=KafkaUtils.createStream(ssc,brokers,"Spark-streaming-consumer",{topic:1})
+    #kvs1=KafkaUtils.createStream(ssc,brokers,"Spark-streaming-consumer",{topic1:1})
+    #print("KVS:",kvs)
+    #print("kvs type:",type(kvs))
+    lines=kvs.map(print_stream)
+    #lines1=kvs1.map(print_stream1)
+    #print("Lines:",lines)
+    lines.pprint()
+    #lines1.pprint()
+    #counts=lines.flatMap(lambda line: line.split(','))
+    #.flatMap(lambda row: (row[0]+10,row[1]+10))
+    #print("COUNTS:")
+    #counts.pprint()
+    
+    #lines[0]=lines[0]+10
+    #lines[0]=lines[1]+10
+    #print("After:")
+    #lines.pprint()
+    #counts=lines.flatMap(lambda line:line.split(" ")).map(lambda word:(word,1)).reduceByKey(lambda a,b:a+b)
+    #counts.pprint()
+
+    ssc.start()
+    ssc.awaitTermination()
